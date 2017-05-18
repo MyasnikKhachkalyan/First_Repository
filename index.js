2cc307e9a286dd671d6f28a06af712da7f4d4766
Myasnik <khachkalyan.myasnik@gmail.com>
2017-05-18 20:08:50 +0400

    ok babygit add index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-'use strick';
-function newF(){
-  console.log("hello baby!!!");
-}
+'use strict';
 
-newF();
+
+const newF = function(arr){
+  const narr = [];
+  for(const i of arr){
+    narr.push(i.toUpperCase());
+  }
+  console.log(i);
+  return narr;
+}
+const res = newF(['foo', 'bar', 'baz']);
+console.log(res);
