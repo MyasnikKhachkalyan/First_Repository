731260cddbfbf11d187c1a06f33596208c4f7f25
Myasnik <khachkalyan.myasnik@gmail.com>
2017-05-18 21:14:25 +0400

    Server VCed

--- ev.js
+++ ev.js
@@ -1,5 +1,33 @@
 'use strict';
+//
+// let counter = 0;
+//
+// const interval = setInterval(function(){
+//   console.log(`called ${++counter} times`);
+//   if (counter === 5) {
+//     clearInterval(interval);
+//   }
+//
+// },300);
 
-setInterval(function(){
-  console.log("hello bab");
-},3000)
+// process.stdin.on('data',function(d){
+//   console.log(d.toString());
+// })
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if(req.url === '/'){
+    res.setHeader('content-type',
+                  'text/html');
+    res.end(`
+      <!doctype html>
+      <body>
+        <h1>some header! </h1>
+        <p> what a body =P </p>
+      </body>
+      `)
+  } else{ res.end('UNKNOWN')}
+})
+
+server.listen(8080, () => console.log("listened to 8080"));
